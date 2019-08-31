96c79963e18634d29e2457acf996959016a6860a
Silas Hallahan <silasishallahan@gmail.com>
2019-08-31 13:24:05 -0700

    login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -16,7 +16,7 @@ AuthRouter
 
     for (const [key, value] of Object.entries(newUser)) {
       if (!value) {
-        res.status(400).json({ 
+        return res.status(400).json({ 
           error: `Missing ${key} in request body` 
         });
       }
@@ -35,6 +35,51 @@ AuthRouter
               .json(AuthServices.serializeUser(user));
           });
       }).catch(next);
+  });
+
+AuthRouter
+  .route('/login')
+  .post(bodyParser, (req, res, next) => {
+    const { username, password } = req.body;
+    const userLogin = { username, password };
+
+    for (const [key, value] of Object.entries(userLogin)) {
+      if (!value) {
+        return res.status(400).json({
+          error: `Missing ${key} in request body`
+        });
+      }
+    }
+
+    AuthServices.getUserWithUsername(req.app.get('db'), userLogin.username)
+      .then(dbUser => {
+        if (!dbUser) {
+          return res.status(400).json({
+            error: 'Incorrect username or password'
+          });
+        }
+
+        return AuthServices.comparePasswords(userLogin.password, dbUser.password)
+          .then(match => {
+            if (!match) {
+              return res.json(400).json({
+                error: 'Incorrect username or password'
+              });
+            }
+
+            const sub = dbUser.username;
+            const payload = { user_id: dbUser.id };
+
+            res.send({
+              authToken: AuthServices.createJwt(sub, payload)
+            });
+          });
+
+      })
+      .catch(err =>{
+        console.error(err);
+        next();
+      });
   });
 
 module.exports = AuthRouter;
