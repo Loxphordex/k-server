96f3c983f5b06e373388a6e03eac6f6fe30aada7
Silas Hallahan <46331012+Loxphordex@users.noreply.github.com>
2019-09-20 09:11:56 -0700

    Update auth-router.js

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -4,38 +4,38 @@ const AuthServices = require('./auth-services');
 const path = require('path');
 const bodyParser = express.json();
 
-AuthRouter
-  .route('/register')
-  .get((req, res) => {
-    res.send('Auth Router');
-  })
-  .post(bodyParser, (req, res, next) => {
-    console.log('ROUTER ACCESSED');
-    const { username, password } = req.body;
-    let newUser = { username, password };
+// AuthRouter
+//   .route('/register')
+//   .get((req, res) => {
+//     res.send('Auth Router');
+//   })
+//   .post(bodyParser, (req, res, next) => {
+//     console.log('ROUTER ACCESSED');
+//     const { username, password } = req.body;
+//     let newUser = { username, password };
 
-    for (const [key, value] of Object.entries(newUser)) {
-      if (!value) {
-        return res.status(400).json({ 
-          error: `Missing ${key} in request body` 
-        });
-      }
-    }
+//     for (const [key, value] of Object.entries(newUser)) {
+//       if (!value) {
+//         return res.status(400).json({ 
+//           error: `Missing ${key} in request body` 
+//         });
+//       }
+//     }
 
-    return AuthServices.hashPassword(newUser.password)
-      .then(hashedPassword => {
-        console.log('PASSWORD HASHED');
-        newUser.password = hashedPassword;
+//     return AuthServices.hashPassword(newUser.password)
+//       .then(hashedPassword => {
+//         console.log('PASSWORD HASHED');
+//         newUser.password = hashedPassword;
 
-        AuthServices.insertUser(req.app.get('db'), newUser)
-          .then(user => {
-            console.log('USER INSERTED');
-            res.status(201)
-              .location(path.posix.join(req.originalUrl, `/${user.id}`))
-              .json(AuthServices.serializeUser(user));
-          });
-      }).catch(next);
-  });
+//         AuthServices.insertUser(req.app.get('db'), newUser)
+//           .then(user => {
+//             console.log('USER INSERTED');
+//             res.status(201)
+//               .location(path.posix.join(req.originalUrl, `/${user.id}`))
+//               .json(AuthServices.serializeUser(user));
+//           });
+//       }).catch(next);
+//   });
 
 AuthRouter
   .route('/login')
