8d1d69fc130fa2f438b9fc9e0f05c90955856035
Silas Hallahan <silasishallahan@gmail.com>
2020-12-04 05:22:26 -0800

    line items debug

--- src/payment/helpers.js
+++ src/payment/helpers.js
@@ -6,23 +6,27 @@ async function mapCartToLineItems(req) {
 
   if (cart) {
     cart.forEach(async (item) => {
+      console.log(item);
       const image = await ImagesServices.getById(req.app.get('db'), item.id);
-      const itemObj = {
-        price_data: {
-          currency,
-          product_data: {
-            name: image.name,
-            images: [image.url]
+      if (image) {
+        const itemObj = {
+          price_data: {
+            currency,
+            product_data: {
+              name: image.name,
+              images: [image.url]
+            },
+            unit_amount: image.price
           },
-          unit_amount: image.price
-        },
-        quantity: item.count
-      };
+          quantity: item.count
+        };
 
-      lineItems.push(itemObj);
+        lineItems.push(itemObj);
+      }
     });
   }
 
+  console.log(lineItems);
   return lineItems;
 }
 
