a289809142831848c15d193e1def558a2de740d1
Silas <silasishallahan@gmail.com>
2021-03-08 14:02:01 -0800

    rm old config vars

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const app = require('./app');
 const {
-  PORT, DATABASE_HOST, DATABASE_NAME, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_URL
+  PORT, DATABASE_URL
 } = require('./config');
 
 const db = knex({
