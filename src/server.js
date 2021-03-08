83de3ec383e53bb0c427b322c81ab12d1a9c76ba
Silas <silasishallahan@gmail.com>
2021-03-08 13:45:23 -0800

    ssl on and db creds

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const knex = require('knex');
 const app = require('./app');
-const { PORT, DATABASE_URL } = require('./config');
+const {
+  PORT, DATABASE_HOST, DATABASE_NAME, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_URL
+} = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    host: DATABASE_HOST,
+    database: DATABASE_NAME,
+    port: DATABASE_PORT,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    ssl: true
+  }
 });
 
 app.set('db', db);
